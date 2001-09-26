7e9f1921a20ccb325bad17e5d22d7943840795fe
Havoc Pennington <hp@src.gnome.org>
2001-09-26 17:59:23 +0000

    remove some straces from script

--- tests/testactivationraces.sh
+++ tests/testactivationraces.sh
@@ -7,22 +7,22 @@ TMPDIR=/tmp/gconftestactivationraces
 
 mkdir $TMPDIR
 
-/bin/rm $TMPDIR/*
+/bin/rm $TMPDIR/* || true
 
 for I in `seq 1 $COUNT`; do
   TMPFILE=`mktemp $TMPDIR/SET.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --type=int --set /test/foo $I &
+  gconftool --type=int --set /test/foo $I &
   TMPFILE=`mktemp $TMPDIR/SHUTDOWN.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --shutdown &
+  gconftool --shutdown &
   TMPFILE=`mktemp $TMPDIR/GET.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --get /test/foo &
+  gconftool --get /test/foo &
   TMPFILE=`mktemp $TMPDIR/SHUTDOWN2.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --shutdown &
+  gconftool --shutdown &
 done
 
 for I in `seq 1 $COUNT`; do
   TMPFILE=`mktemp $TMPDIR/SET2.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --type=int --set /test/foo $I &
+  gconftool --type=int --set /test/foo $I &
   TMPFILE=`mktemp $TMPDIR/SHUTDOWN3.$I.XXXXXX` || exit 1
-  strace -o $TMPFILE gconftool --shutdown &
+  gconftool --shutdown &
 done
